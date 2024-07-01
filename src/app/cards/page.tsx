8e736a7f7236c09a3ef3c7c3589c8cff7bc00d08
wii <99741633+imyanglu@users.noreply.github.com>
2024-07-02 03:38:14 +0800

    i

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,6 +1,11 @@
 const Page = () => {
-    return <div>
-        query
+    return <div className="w-[100vw]">
+        <div className="recommendCard w-full py-[200px]">
+            <div>
+              
+            </div>
+        </div>
+  
     </div>
  }
 export default Page
