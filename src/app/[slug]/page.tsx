2a3226a49f34c4ef9859be3df84a3b7a7e2e6003
wii <99741633+imyanglu@users.noreply.github.com>
2024-07-17 01:34:33 +0800

    i

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,4 @@
+const Page = () => {
+  return <div>建设ing</div>;
+};
+export default Page;
--- src/app/cards/[slug]/page.tsx
+++ src/app/cards/[slug]/page.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import { getCardGroup } from '@/app/api';
-import { DeckContainer } from '@/app/components';
+import { DeckContainer, Title } from '@/app/components';
 import { JobsData } from '@/app/Const';
 import { Card, HsCard } from '@/type';
 import Header from './components/Header';
@@ -18,18 +19,87 @@ const Page = async ({ params: { slug } }: { params: { slug: string } }) => {
     groupMap.set(card.id, { ...card, count: 1 });
   });
 
+  const regularCards = Array.from(groupMap.values()).filter((a) => a.faction === 'neutral');
+  const decks = Array.from(groupMap.values()).filter((a) => a.faction !== 'neutral');
+  const hero = JobsData.find((a) => a.slug === decks[0].faction);
+
   return (
     <div className="bg-[#372B47] w-[100vw] flex flex-col h-[100vh]">
       <div className="h-[80px] w-full">
         <Header code={card.code} />
       </div>
-      <div className="xs:block  my-[20px] min-h-[500px]">
-        <DeckContainer
-          thumbnail={JobsData.find((a) => a.slug === card.type)!.thumbnail}
-          mode="show"
-          defaultName={card.name}
-          selectedCards={groupMap}
-        />
+      <div className="flex px-[24px] h-[calc(100vh-80px)]">
+        <div className="overflow-y-scroll flex-1 hideScrollbar   hideScrollbar hidden md:flex flex-col pr-[24px]">
+          <div className="w-full">
+            {hero && (
+              <Title
+                type={hero.slug}
+                label={
+                  <div className="text-[#fff] w-[fit-content] translate-y-[-2px] font-bold text-[16px]">
+                    {hero.name}
+                  </div>
+                }
+              />
+            )}
+          </div>
+          <div className="grid grid-cols-[repeat(auto-fit,_minmax(220px,_1fr))] w-full gap-[12px]   px-[12px]">
+            {decks.map((card) => (
+              <div
+                key={card.id}
+                data-id={card.id}
+                className="w-[240px]  cursor-pointer aspect-[202/279]"
+                draggable>
+                <img
+                  data-id={card.id}
+                  className="w-full "
+                  src={card.img}
+                  alt={card.name}
+                  loading="lazy"
+                  decoding="async"
+                />
+              </div>
+            ))}
+          </div>
+
+          <div className="w-full">
+            {regularCards.length > 0 && (
+              <Title
+                type="neutral"
+                label={
+                  <div className="text-[#fff] w-[fit-content] translate-y-[-2px] font-bold text-[16px]">
+                    中立卡牌
+                  </div>
+                }
+              />
+            )}
+          </div>
+          <div className="grid grid-cols-[repeat(auto-fit,_minmax(220px,_1fr))] gap-[12px] w-full   px-[12px]">
+            {regularCards.map((card) => (
+              <div
+                key={card.id}
+                data-id={card.id}
+                className="w-[240px]  cursor-pointer aspect-[202/279]"
+                draggable>
+                <img
+                  data-id={card.id}
+                  className="w-full "
+                  src={card.img}
+                  alt={card.name}
+                  loading="lazy"
+                  decoding="async"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="xs:block mx-auto py-[20px] min-h-[500px]">
+          <DeckContainer
+            thumbnail={JobsData.find((a) => a.slug === card.type)!.thumbnail}
+            mode="show"
+            defaultName={card.name}
+            selectedCards={groupMap}
+          />
+        </div>
       </div>
     </div>
   );
