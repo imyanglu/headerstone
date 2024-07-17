4d51c39d649c0dff3f803a81d95f0b4ae8f6f763
wii <99741633+imyanglu@users.noreply.github.com>
2024-07-18 01:49:31 +0800

    i

--- src/app/cards/conponents/CardItem.tsx
+++ src/app/cards/conponents/CardItem.tsx
@@ -11,8 +11,8 @@ const CardItem = (data: CardGroupOverview) => {
     <>
       <div className="py-[12px] flex w-full items-center">
         <img className=" w-[32px] h-[32px] rounded-full" src={hero.weapon} alt="" />
-        <div className="w-[100px] ml-[16px]">
-          <div className="font-bold">{name}</div>
+        <div className="w-[60px] sm:w-[100px] ml-[16px]">
+          <div className="font-bold line-clamp-1 break-all">{name}</div>
           <div className="flex items-center mt-[4px]">
             <img
               className="w-[12px] h-[17px]"
@@ -22,10 +22,10 @@ const CardItem = (data: CardGroupOverview) => {
             <p className="text-[12px] ml-[4px]">5240</p>
           </div>
         </div>
-        <div className="">
+        <div className="ml-[8px] text-[12px] sm:text-[16px]">
           胜率:&nbsp;<strong className="text-[rgb(31,173,30)]">{winningRate}%</strong>
         </div>
-        <div className="border-[4px] w-[200px] ml-auto mt-[4px] shadow-lg outline-[#e1c892] outline-[5px] outline border-[#000]  relative z-[2] rounded-[10px]">
+        <div className="border-[4px] w-[100px] md:w-[200px] ml-auto mt-[4px] shadow-lg outline-[#e1c892] outline-[5px] outline border-[#000]  relative z-[2] rounded-[10px]">
           <div className=" outline-[#E3D07F] flex items-center outline outline-[3px] pl-[8px] text-[14px]  py-[3px]  rounded-[10px] border-[2px] border-[#000]  bg-[#3D0D0D] text-[#fff]">
             <div className="flex-1 line-clamp-1 break-all">{code}</div>
             {code && <CopyBtn code={code} />}
--- src/app/cards/conponents/ClientSection.tsx
+++ src/app/cards/conponents/ClientSection.tsx
@@ -5,8 +5,8 @@ import CardItem from './CardItem';
 import CardGroup from './CardGroup';
 const ClientSection = ({ decks }: { decks: (CardGroupOverview & { pic: string })[] }) => {
   return (
-    <div className="w-[100vw] bg-[#76191A] flex flex-col min-h-[100vh] ">
-      <div className="w-full h-[100px] relative flex items-center bg-[#561212] ">
+    <div className="w-[100vw] bg-[#76191A] flex flex-col min-h-[100vh] ]">
+      <div className="fixed top-0 w-full h-[100px] flex items-center bg-[#561212] ">
         <div className="absolute top-[1px] z-[10] h-[30px] w-full ">
           <Image src="https://pic.imgdb.cn/item/6683b127d9c307b7e99abe59.png" alt="" fill />
         </div>
@@ -16,7 +16,9 @@ const ClientSection = ({ decks }: { decks: (CardGroupOverview & { pic: string })
         <div className="absolute bottom-0 h-[20px] w-full">
           <Image src="https://pic.imgdb.cn/item/6683b07cd9c307b7e998b6a1.png" alt="" fill />
         </div>
-        <a href="/" className="relative z-2 px-[24px] text-[#fff] font-bold text-[26px]">
+        <a
+          href="/"
+          className="relative z-2 px-[24px] text-[#fff] font-bold text-[16px] md:text-[26px]">
           LSCX.Xyz
         </a>
         <div className="hidden md:block border-[4px] shadow-lg outline-[#7c221f] outline-[5px] outline border-[#000] ml-[12px] relative z-[2] rounded-[8px]">
@@ -24,11 +26,11 @@ const ClientSection = ({ decks }: { decks: (CardGroupOverview & { pic: string })
             卡组查询
           </div>
         </div>
-        <div className="ml-[32px] mr-[24px] border-[3px] shadow-lg outline-[#7c221f] outline-[5px] outline border-[#000]  relative z-[20] rounded-[12px]">
+        <div className="ml-[32px] mr-[12px] md:mr-[24px] border-[3px] shadow-lg outline-[#7c221f] outline-[5px] outline border-[#000]  relative z-[20] rounded-[12px]">
           <div className=" outline-[#E3D07F] flex items-center outline outline-[3px] px-[16px] text-[14px]  py-[3px]  rounded-[12px] border-[2px] border-[#000]  bg-[#3D0D0D] text-[rgb(97,67,38)]">
             <input
               placeholder="搜索卡组..."
-              className="outline-none text-[14px] w-[180px]  py-[4px]  font-bold bg-[#3D0D0D] text-[#fff]"
+              className="outline-none text-[14px] w-full md:w-[180px]  py-[4px]  font-bold bg-[#3D0D0D] text-[#fff]"
             />
             <div className="h-[30px] w-[30px] cursor-pointer absolute right-[10px]  flex items-center justify-end">
               <img src="/search.svg" alt="search" className="w-[24px] h-[24px]" />
@@ -36,12 +38,13 @@ const ClientSection = ({ decks }: { decks: (CardGroupOverview & { pic: string })
           </div>
         </div>
       </div>
-      <div className="flex-1 mainSection flex">
-        <div className="w-[300px] shrink-0 bg-[#531211]">
+      <div className="h-[100px]" />
+      <div className="flex-1 mainSection flex flex-col lg:flex-row">
+        <div className="lg:w-[300px] w-full shrink-0 bg-[#531211] pb-[20px]">
           <div className="text-[#fff] font-bold text-center py-[16px]">卡组推荐</div>
           <CardGroup {...decks[0]} />
         </div>
-        <div className="flex flex-col px-[16px] w-full">
+        <div className="flex flex-col px-[16px] w-full pb-[60px]">
           {decks.map((d) => (
             <CardItem key={d.id} {...d} />
           ))}
