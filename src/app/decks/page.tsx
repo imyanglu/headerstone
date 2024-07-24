7b4dbe3ed49915e84c1afac57d5c02ec4fd9169b
wii <99741633+imyanglu@users.noreply.github.com>
2024-07-24 16:14:39 +0800

    i

--- src/app/decks/page.tsx
+++ src/app/decks/page.tsx
@@ -6,24 +6,34 @@ export const runtime = 'edge';
 const ClientSection = dynamic(() => import('./conponents/ClientSection'), { ssr: false });
 const baseUrl = 'https://8.138.99.181:3000/recommendOverview';
 const fetchData = async () => {
-  const cardGroupResult = await fetch(baseUrl, {
-    cache: 'no-cache',
-  }).then((d) => d.json() as Promise<{ cards: CardGroupOverview[] }>);
-  const cardsMap = cardGroupResult.cards.map((a) => ({
-    ...a,
-    pic: JobsData.find((i) => i.slug === a.type)?.pic ?? '',
-  }));
-  return cardsMap;
+  try {
+    const cardGroupResult = await fetch(baseUrl, {
+      cache: 'no-cache',
+    }).then((d) => d.json() as Promise<{ cards: CardGroupOverview[] }>);
+    const cardsMap = cardGroupResult.cards.map((a) => ({
+      ...a,
+      pic: JobsData.find((i) => i.slug === a.type)?.pic ?? '',
+    }));
+    return cardsMap;
+  } catch (err) {
+    console.log(err, 'reslut');
+    return [];
+  }
 };
 const fetchPreview = async () => {
-  const cardGroupResult = await fetch(baseUrl + '?preview=true', {
-    cache: 'no-cache',
-  }).then((d) => d.json() as Promise<{ cards: CardGroupOverview[] }>);
-  const cardsMap = cardGroupResult.cards.map((a) => ({
-    ...a,
-    pic: JobsData.find((i) => i.slug === a.type)?.pic ?? '',
-  }));
-  return cardsMap;
+  try {
+    const cardGroupResult = await fetch(baseUrl + '?preview=true', {
+      cache: 'no-cache',
+    }).then((d) => d.json() as Promise<{ cards: CardGroupOverview[] }>);
+    const cardsMap = cardGroupResult.cards.map((a) => ({
+      ...a,
+      pic: JobsData.find((i) => i.slug === a.type)?.pic ?? '',
+    }));
+    return cardsMap;
+  } catch (err) {
+    console.log(err, 'preview');
+    return [];
+  }
 };
 
 const Page = async () => {
