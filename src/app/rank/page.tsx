c28bf292127ef644568b3e1b60e24f150a279dae
wii <99741633+imyanglu@users.noreply.github.com>
2024-08-06 08:03:46 +0800

    i

--- src/app/rank/page.tsx
+++ src/app/rank/page.tsx
@@ -1,40 +1,20 @@
-'use client';
-
-import { useEffect, useMemo, useState } from 'react';
-import useToast from '../lib/hooks';
-import Toast from '../components/Toast';
 import { getRankList } from '../api';
-import { AutoSizer, List } from 'react-virtualized';
+import MainSection from './MainSection';
 import Image from 'next/image';
-const Page = () => {
-  const { addToast } = useToast();
-  const [s, setS] = useState('');
-  const [rankList, setRankList] = useState<{
-    pagination: { totalPages: number; totalSize: number };
-    users: Array<{ idx: number; name: string }>;
-  } | null>(null);
 
-  const initRankList = async () => {
-    getRankList()
-      .then((d) => {
-        const pagination = d.data.rankList.pagination;
-        const users = d.data.rankList.users;
-        setRankList({ pagination, users: users.map((u, idx) => ({ idx: idx + 1, name: u })) });
-      })
-      .catch((e) => {
-        addToast({ type: 'error', message: '加载数据失败!', title: '' });
-      });
-  };
+const initRankList = async () => {
+  try {
+    const d = await getRankList();
+    const pagination = d.data.rankList.pagination;
+    const users = d.data.rankList.users;
+    return { pagination, users: users.map((u, idx) => ({ idx: idx + 1, name: u })) };
+  } catch (err) {
+    return null;
+  }
+};
 
-  const processUsers = useMemo(() => {
-    const users = rankList?.users ?? [];
-    return users.filter((u) => u.name.includes(s));
-  }, [s, rankList]);
-
-  useEffect(() => {
-    initRankList();
-  }, []);
-
+const Page = async () => {
+  const data = await initRankList();
   return (
     <div className="h-[100vh] bg-[#E8D4A8]  flex flex-col">
       <div className="fixed top-0 z-[10] w-full h-[100px] flex items-center bg-[#561212] ">
@@ -71,60 +51,7 @@ const Page = () => {
         </div>
       </div>
       <div className="h-[120px]"></div>
-      <div className="mb-[24px]">
-        <div className="w-[300px] mx-auto outline-[#c2b085] flex items-center outline outline-[3px] px-[16px] text-[14px]  py-[3px]  rounded-[24px] border-[2px] border-[#000]  bg-[#E8D4A8] text-[rgb(97,67,38)]">
-          <input
-            placeholder="搜索..."
-            className="outline-none text-[14px]  py-[3px]  font-bold bg-[#E8D4A8] text-[#65482A]"
-            onChange={(e) => {
-              setS(e.target.value);
-            }}
-          />
-          <div
-            className="h-[30px] w-[30px] cursor-pointer absolute right-[10px]  flex items-center justify-end"
-            onClick={() => {}}>
-            <img src="/search.svg" alt="search" className="w-[24px] h-[24px]" />
-          </div>
-        </div>
-      </div>
-
-      <div className="h-full flex-1 max-w-[1000px] mx-auto shrink-0 min-h-[300px] w-[80%] ">
-        <AutoSizer>
-          {({ height, width }) => {
-            if (!rankList)
-              return (
-                <div className="text-center font-bold" style={{ width }}>
-                  加载中...
-                </div>
-              );
-            return (
-              <List
-                rowCount={processUsers.length || 0}
-                rowHeight={60}
-                height={height}
-                width={width}
-                rowRenderer={({ index, style, key }) => {
-                  const u = processUsers[index];
-                  return (
-                    <div
-                      style={{ ...style, backgroundColor: index % 2 === 0 ? '#D9C69A' : '#E8D4A8' }}
-                      key={key}
-                      className="h-[60px] flex items-center px-[24px]">
-                      <div className="relative w-[50px] h-[50px]">
-                        <Image src="/std-rank.avif" fill alt="" />
-                        <div className="inset-0 font-bold leading-[50px] text-center text-[#fff] stroke absolute">
-                          {u?.idx}
-                        </div>
-                      </div>
-                      <div className="font-bold ml-[12px] text-[#65482A]">{u?.name}</div>
-                    </div>
-                  );
-                }}
-              />
-            );
-          }}
-        </AutoSizer>
-      </div>
+      <MainSection rankList={data} />
     </div>
   );
 };
