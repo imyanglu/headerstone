7009eac9eacfb9ed7274d6b84098c2fdae33ec69
wii <99741633+imyanglu@users.noreply.github.com>
2024-08-07 11:00:04 +0800

    i

--- src/app/lib/help.ts
+++ src/app/lib/help.ts
@@ -1,6 +1,7 @@
-import { decode, FormatType, encode } from 'deckstrings';
+import { decode, FormatType, encode, DeckCard } from 'deckstrings';
 import { JobsData } from '../Const';
 import { Heros, StandardCards } from './data';
+import { HsCard } from '@/type';
 export const getImgSrc = (id: string) => {
   return `https://art.hearthstonejson.com/v1/render/latest/zhCN/512x/${id}.png`;
 };
@@ -29,9 +30,16 @@ export const decodeCode = (text: string) => {
   const code = codeMatch ? 'AAEC' + codeMatch[0] : null;
   if (!code) throw new Error('无法解析套牌代码');
   const result = decode(code);
-  const cardIds = result.cards.map((a) => Array.from({ length: a[1] }, () => a[0])).flat(2);
-  const ids = StandardCards.filter((a) => cardIds.includes(a.dbfId)).map((a) => a.id);
-  console.log(ids);
+  const cardsArr = result.cards;
 
-  return { code, title };
+  const hero = Heros.find((a) => a.dbfId === result.heroes[0]);
+
+  const cardIds = result.cards.map((a) => Array.from({ length: a[1] }, () => a[0])).flat(2);
+  const cardMap = new Map<string, HsCard & { count: number }>();
+  StandardCards.filter((a) => cardIds.includes(a.dbfId)).forEach((i) => {
+    const count = cardsArr.find((item) => item[0] === i.dbfId)?.[1] ?? 1;
+    cardMap.set(i.id, { ...i, count });
+  });
+
+  return { code, title, cards: cardMap, hero: (hero?.cardClass ?? '').toLowerCase() };
 };
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -10,39 +10,6 @@ import { decodeCode } from '../lib/help';
 const Heros = JobsData;
 
 const Page = () => {
-  const inputRef = useRef();
-  const submit = () => {};
-  useEffect(() => {
-    const result = decodeCode(`
-### 铺场骑
-# 职业：圣骑士
-# 模式：标准模式
-#
-# 2x (1) 奇迹推销员
-# 2x (1) 正义保护者
-# 2x (1) 鱼人木乃伊
-# 2x (2) 威猛银翼巨龙
-# 2x (2) 淘金客
-# 2x (2) 采矿事故
-# 2x (3) 作战动员
-# 2x (3) 决战！
-# 1x (3) 戈贡佐姆
-# 2x (4) 光速抢购
-# 2x (4) 十字军光环
-# 1x (4) 布吉舞乐
-# 1x (4) 音响工程师普兹克
-# 1x (5) 玩具队长塔林姆
-# 1x (7) 维和者阿米图斯
-# 1x (8) 奇利亚斯豪华版3000型
-#   1x (0) 奇利亚斯豪华版3000型
-#   1x (3) 输能模块
-#   1x (5) 计数模块
-# 2x (8) 棱彩光束
-# 2x (10) 海巨人
-AAECAZ8FBsHEBfTIBY3+BcekBtOpBrrOBgzJoASi1ASU9QWV9QWFjgaZjga8jwb1lQbOnAbUngaSoAbeugYAAQPzswbHpAb2swbHpAbo3gbHpAYAAA==
-`);
-    console.log(result);
-  }, []);
   return (
     <div className="w-[100vw]  relative min-h-[100vh] p-[24px] bg-[#711B1C]">
       <div className="w-[100vw] bottom-0 bg-[#000000] z-[-2]  absolute top-0 left-0">
@@ -60,15 +27,7 @@ AAECAZ8FBsHEBfTIBY3+BcekBtOpBrrOBgzJoASi1ASU9QWV9QWFjgaZjga8jwb1lQbOnAbUngaSoAbe
           标准卡组
         </div>
       </div>
-      <div className="border-[2px] w-[fit-content] shadow-lg outline-[#500f10] outline-[2px] ml-[24px] outline border-[#501516]  relative z-[2] rounded-[12px]">
-        <div className=" outline-[#c2b085] flex items-center outline outline-[3px] px-[16px] text-[14px]  py-[3px]  rounded-[12px] border-[2px] border-[#000]  bg-[#3D0D0D] text-[rgb(97,67,38)]">
-          <input
-            placeholder="代码..."
-            className="outline-none text-[14px]  py-[5px]  font-bold bg-[#3D0D0D] text-[#fff]"
-            onChange={(e) => {}}
-          />
-        </div>
-      </div>
+
       <div className="grid grid-cols-2 mt-[24px] md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-[8px] sm:gap-[20px]">
         {Heros.map((item, index) => {
           return (
